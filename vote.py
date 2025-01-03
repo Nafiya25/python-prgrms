68b56c5995b8476aab8d8663c12c0714021b1e36
Nafiya25 <nafyaaboobakkar25@gmail.com>
2025-01-03 20:54:29 +0530

    vote.py

--- vote.py
+++ vote.py
@@ -1,4 +1,4 @@
-#Write a program to find the largest of three numbers.
+#Write a program to check if a person is eligible to vote.
 age = int(input("Enter your age: "))
 if age >+18:
     print("You are eligible to vote")
